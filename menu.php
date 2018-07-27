cd3225010b4f7a1056a78d4568cc8a3e13ae43b7
taskinoz <tristan@taskinoz.com>
2018-07-27 20:09:02 +1000

    Changed menu order

--- menu.php
+++ menu.php
@@ -39,10 +39,10 @@
     <div class="nav-wrapper">
       <a class="brand-logo center" href="/"><img class="logo" src="/img/trends_logo.png" alt="Google trends Logo" title="Google trends Logo"></a>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a class="dropdown-trigger" href="#!" data-target="dropdown1">Trends<i class="fa fa-sort-down right"></i></a></li>
         <li><a href="about/">About</a></li>
         <li><a href="rules/">Rules</a></li>
         <li><a href="contact/">Contact</a></li>
-        <li><a class="dropdown-trigger" href="#!" data-target="dropdown1">Trends<i class="fa fa-sort-down right"></i></a></li>
       </ul>
     </div>
   </nav>
