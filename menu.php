17178e930170e87ca2b2763015d7b905052a4efa
taskinoz <tristan@taskinoz.com>
2018-09-21 00:14:26 +1000

    Fixed redirect error

--- menu.php
+++ menu.php
@@ -55,9 +55,9 @@
     <div class="nav-wrapper">
       <a class="brand-logo center" href="/"><img class="logo" src="/img/trends_logo.png" alt="Google trends Logo" title="Google trends Logo"></a>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="about/">About</a></li>
-        <li><a href="rules/">Rules</a></li>
-        <li><a href="contact/">Contact</a></li>
+        <li><a href="/about/">About</a></li>
+        <li><a href="/rules/">Rules</a></li>
+        <li><a href="/contact/">Contact</a></li>
         <li><a class="dropdown-trigger" href="#!" data-target="dropdown1">Trends<i class="fa fa-sort-down right"></i></a></li>
         <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="fa fa-bars"></i></a>
       </ul>
