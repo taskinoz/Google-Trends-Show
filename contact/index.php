1afb200e798f5b5d059e1a9ac2337a2c3e8229f2
taskinoz <tristan@taskinoz.com>
2018-09-21 00:24:27 +1000

    Added Rules page Added Contact page

---
+++ contact/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>Google Trends Show | About</title>
+      <meta name="description" content="">
+      <meta name="keywords" content="google,trends,show,description,about,funhaus,roosterteeth">
+      <meta name="author" content="Trend Master">
+      <?php include '../scripts.php';?>
+    </head>
+    <body>
+      <?php include '../menu.php';?>
+      <div class="container">
+        <div class="row">
+          <div class="col s12">
+            <h1>Contact</h1>
+            <h2>Twitter</h2>
+            <p><a href="https://twitter.com/taskinoz"></a>Follow me</p>
+            <?php // TODO: Add contact form ?>
+          </div>
+        </div>
+      </div>
+      <?php include '../footer.php';?>
+    </body>
+  </html>
---
+++ rules/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>Google Trends Show | About</title>
+      <meta name="description" content="">
+      <meta name="keywords" content="google,trends,show,description,about,funhaus,roosterteeth">
+      <meta name="author" content="Trend Master">
+      <?php include '../scripts.php';?>
+    </head>
+    <body>
+      <?php include '../menu.php';?>
+      <div class="container">
+        <div class="row">
+          <div class="col s12">
+            <h1>Rules</h1>
+            <iframe src="https://www.youtube.com/embed/OzupzEMuPQE" class="yt" width="560" height="315" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+            <?php // TODO: Add list of rules ?>
+          </div>
+        </div>
+      </div>
+      <?php include '../footer.php';?>
+    </body>
+  </html>
