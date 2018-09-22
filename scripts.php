788c35b97e95621254d2287e5039ace928c48e5b
taskinoz <tristan@taskinoz.com>
2018-09-23 01:36:28 +1000

    Removed local materialize files and replaced them with CDN links

--- scripts.php
+++ scripts.php
@@ -3,8 +3,10 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <!-- Materialize -->
-  <link rel="stylesheet" href="/css/materialize.min.css">
-  <script src="/js/materialize.min.js" charset="utf-8"></script>
+  <!-- Compiled and minified CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+  <!-- Compiled and minified JavaScript -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
   <!-- End Materialize -->
   <link rel="stylesheet" href="/css/style.css">
   <script src="/js/trends.js" charset="utf-8"></script>
